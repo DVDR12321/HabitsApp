3aa6018de814e8f77a24c350d8b31033a97777a3
unknown <danilovukovic.95@gmail.com>
2023-04-07 17:21:23 +0200

    First commit, testing

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Text>This is my first ReactNative app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f85c13',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color:"#fff123",
+  },
+});

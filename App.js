85c111e66880450f2ab6f8ef721d41e3e1c60658
unknown <danilovukovic.95@gmail.com>
2023-04-10 21:55:20 +0200

    testing with marko

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Hello World!</Text>
       <Text>This is my first ReactNative app!</Text>
+      <Text>Provera za Marka Provera za Marka Provera za Marka</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f85c13',
+    backgroundColor: '#17f032',
     alignItems: 'center',
     justifyContent: 'center',
     color:"#fff123",
